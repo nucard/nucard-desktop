4312a6c6eace4666b718c9d09d3471e62e3afa5b
ben <ben@jammerware.com>
2018-07-20 21:26:10 -0400

    Started laying out extensions in preferences.

--- src/modules/extensions/components/extensions/extensions.component.ts
+++ src/modules/extensions/components/extensions/extensions.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { NcExtension } from '@nucard/models';
+import { Observable } from 'rxjs';
+import { NcApiService } from '../../../shared/services/nc-api.service';
 
 @Component({
-  selector: 'nc-extensions',
-  templateUrl: './extensions.component.html',
-  styleUrls: ['./extensions.component.scss']
+    selector: 'nc-extensions',
+    templateUrl: './extensions.component.html',
+    styleUrls: ['./extensions.component.scss']
 })
 export class ExtensionsComponent implements OnInit {
+    public userExtensions: Observable<NcExtension[]> = null;
 
-  constructor() { }
+    constructor(private apiService: NcApiService) { }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.userExtensions = this.apiService.getExtensions();
+    }
 }
--- src/modules/extensions/extensions.module.ts
+++ src/modules/extensions/extensions.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppMaterialModule } from '../app-material/app-material.module';
 import { SharedModule } from '../shared/shared.module';
 
 import { ExtensionsComponent } from './components/extensions/extensions.component';
@@ -7,6 +8,7 @@ import { ExtensionsComponent } from './components/extensions/extensions.componen
 @NgModule({
     imports: [
         CommonModule,
+        AppMaterialModule,
         SharedModule,
     ],
     declarations: [ExtensionsComponent],
